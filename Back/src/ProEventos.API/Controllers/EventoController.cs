69fc1ae4e5c15f3a2e000aa3dc9ee0edd06777a9
Rodrigo Rocha <rodrigovieirablack@gmail.com>
2025-05-24 15:41:16 -0300

    Adicionando EntityFrameworkCore

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,42 +14,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private readonly IEnumerable<Evento> _evento = new Evento[]
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-            new Evento()
-            {
-                EventoId = 1,
-                Local = "São Paulo",
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                Tema = "Angular e .NET",
-                QtdPessoas = 250,
-                Lote = "1º Lote",
-                ImagemURL = "http://www.google.com"
-            },
-            new Evento()
-            {
-                EventoId = 2,
-                Local = "Rio de Janeiro",
-                DataEvento = DateTime.Now.AddDays(3).ToString(),
-                Tema = "Angular e .NET",
-                QtdPessoas = 250,
-                Lote = "1º Lote",
-                ImagemURL = "http://www.google.com"
-            }
-        };
-        public EventoController()
-        {
+            _context = context;
         }
+
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos.ToList();
         }
         [HttpGet("{id:int}")]
         public Evento GetById(int id)
         {
-            return _evento.FirstOrDefault(e => e.EventoId == id);
+            return  _context.Eventos.Find(id);
         }
+        
     }
 
 }
